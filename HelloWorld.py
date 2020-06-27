a73c613964c766bc6d70ee47b64c877e3e47cfc2
Kyle Connelly <kmconnel@ncsu.edu>
2020-06-26 20:11:57 -0400

    Commit message

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,3 +3,5 @@ print("Hello, quantum world!")
 print("Testing commit rules!!!!")
 print("For real this time")
 print("Kyle was here, but he wasn't sure how fast he was going")
+
+print("Yet another test of GitHub")
