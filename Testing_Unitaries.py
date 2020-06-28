31a8005dfa2463562db4dd5e30f7f292c6d6c88e
Kyle Connelly <kmconnel@ncsu.edu>
2020-06-28 13:45:53 -0400

    debugged & simplified

--- Testing_Unitaries.py
+++ Testing_Unitaries.py
@@ -4,15 +4,18 @@ Created on Sat Jun 27 17:15:34 2020
 
 @author: Thoma
 """
-
+# %% codecell
+# imports
 import numpy as np
 import math
 import numpy.linalg as la
 import scipy.linalg as scila
 from numpy import kron
 
-#Adjaceny standard of Aij is a line from i to j
-#kronBuilder can be fixed
+# %% codecell
+# setup functions
+
+#Adjacency standard of Aij is a line from i to j
 
 def indegrees(M,node):
     #This is the sum of the colume at node X, counting from 0
@@ -33,68 +36,58 @@ def aVal(M,x):
     else:
         return indegree / (indegree + outdegree)
 
-def kronBuilder(stringElement,length):
-    stringElement = stringElement.zfill(length)
-    kronReturn = 1
-    kronIndex = [[1,0],[0,1]]
-    for item in range(length):
-        kronReturn = kron(kronIndex[int(stringElement[item])],kronReturn)
-    return kronReturn
-    #print(kronReturn)
-
 def constructC(M):
-    dimensions = math.ceil(np.log2(M.shape[0]))
-    output = np.zeros([2 * x for x in A.shape])
+
+    # Force input matrix to already have dimensions as a power of 2
+    if not np.isclose(np.log2(M.shape[0]), int(np.log2(M.shape[0]))):
+        print("Please pad your adjacency matrix so its dimensions are a power of 2")
+
+    output = np.zeros([2 * x for x in M.shape])
 
     for x in range(M.shape[0]):
+
         xVec = np.zeros(A.shape[0])
         xVec[x] = 1
-        # print(xVec)
-        outerX = np.outer(xVec,xVec)
+        outerX = np.diag(xVec)
+
         a = aVal(M,x)
-        # print(a)
-        if a != -1:
-            cellElement = np.array([[np.sqrt(1/(a + 1)),np.sqrt(a/(1 + a))],[np.sqrt(a/(1 + a)),-1 * np.sqrt(1/(a + 1))]])
-            output += kron(cellElement,outerX)
+        cellElement = np.array([[np.sqrt(1/(a + 1)),np.sqrt(a/(1 + a))],[np.sqrt(a/(1 + a)),-1 * np.sqrt(1/(a + 1))]])
+        output += kron(cellElement,outerX)
+
     return output
 
+def constructS(M):
 
-#WIP
-def constructS(M,U):
-    dimensions = math.ceil(np.log2(M.shape[0]))
-    output = np.zeros([2 * x for x in A.shape],dtype=np.complex128)
-    up = np.array([1,0])
-    down = np.array([0,1])
+    # Force input matrix to already have dimensions as a power of 2
+    if not np.isclose(np.log2(M.shape[0]), int(np.log2(M.shape[0]))):
+        print("Please pad your adjacency matrix so its dimensions are a power of 2")
 
-    for x in range(M.shape[0]):
-        xVec = np.zeros(A.shape[0])
-        xVec[x] = 1
-        outerX = np.outer(xVec, xVec)
-        firstTerm = kron(np.outer(up,up),outerX)
-        secondTermOutput = np.zeros([2 * x for x in A.shape],dtype=np.complex128)
-        for k in range(M.shape[0]):
-            kVec = np.zeros(A.shape[0])
-            kVec[k] = 1
-            kOuterX = np.outer(kVec,xVec)
-            outerDown = np.outer(down,down)
-            #CHECK THESE COORDS
-            secondTermOutput += kron(outerDown,U[k][x] * kOuterX)
-        output += firstTerm
-        output += secondTermOutput
-    return output
+    up_proj = np.diag([1,0])
+    down_proj = np.diag([0,1])
 
+    P,D,Q = la.svd(M)
+    U = P @ scila.expm(1j*np.diag(D)) @ Q
 
-#For the test case:
-A = np.array([[0,1,0,0,1,1,0,0],[0,0,0,0,0,0,0,0],[1,1,0,0,0,0,1,0],[0,0,1,0,1,1,0,0],[0,0,0,0,0,0,1,0],[0,0,0,1,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,0]])
-PDQ = la.svd(A)
-P = PDQ[0]
-D = np.diag(PDQ[1])
-Q = PDQ[2]
-U = P @ scila.expm(1j*D) @ Q
+    if_up = np.kron(up_proj, np.identity(M.shape[0]))
+    if_down = np.kron(down_proj, U)
+    S = if_up+if_down
 
-#Finding C
-C = constructC(A)
-C = np.matrix(C)
+    return S
 
-S = constructS(A,U)
-S = np.matrix(S)
+# # %% codecell
+# #For the test case:
+# A = np.array([  [0,1,0,0,1,1,0,0],
+#                 [0,0,0,0,0,0,0,0],
+#                 [1,1,0,0,0,0,1,0],
+#                 [0,0,1,0,1,1,0,0],
+#                 [0,0,0,0,0,0,1,0],
+#                 [0,0,0,1,0,0,0,0],
+#                 [0,0,0,0,0,1,0,0],
+#                 [0,0,0,0,0,0,0,0]])
+# #Finding C
+# C = constructC(A)
+# C = np.matrix(C)
+#
+# S = constructS(A)
+# S = np.matrix(S)
+#
