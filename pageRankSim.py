9fd37ebe6b0089cb4c5fd2a087a9a2a245ad1553
Kyle Connelly <kmconnel@ncsu.edu>
2020-06-28 16:12:09 -0400

    Commented and cleaned up

--- pageRankSim.py
+++ pageRankSim.py
@@ -8,10 +8,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg as la
 import Testing_Unitaries
+######################
+# MAKE SURE Testing_Unitaries IS UP TO DATE!
+######################
 
 # %% codecell
+# create the operators
 
-# adjacency matrix
+# input adjacency matrix
 A = np.array([  [0,1,0,0,1,1,0,0],
                 [0,0,0,0,0,0,0,0],
                 [1,1,0,0,0,0,1,0],
@@ -21,66 +25,60 @@ A = np.array([  [0,1,0,0,1,1,0,0],
                 [0,0,0,0,0,1,0,0],
                 [0,0,0,0,0,0,0,0]])
 
+# number of qubits needed for graph
+n_qubits = int(np.log2(A.shape[0]))
+
 # make C and S operators
 C = Testing_Unitaries.constructC(A)
-# C = np.matrix(C)
-
 c_gate = UnitaryGate(C)
 
 S = Testing_Unitaries.constructS(A)
-# S = np.matrix(S)
-
 s_gate = UnitaryGate(S)
-
-########################
-# I don't think this block works, but it may be useful later
-########################
-
-# # %% codecell
-# # make a circuit with this gate
-#
-# testckt=unit_gate.definition
-# testckt.name = 'test'
-# testckt.draw(output='text')
-########################
 
 # %% codecell
 # run circuit
-
-# I'm running this for an absurdly long time
 
 # initialize to superposition without |up>|111> or |down>|111>
 init_vector = np.ones(16)/np.sqrt(14)
 init_vector[7] = init_vector[15] = 0
 
 # run the circuits for many timesteps and average the results
+# the paper started at 50 and ran up to 200-500, depending on the size of the
+# graph
+# I'm doing it in steps of 10
 runs = [10*i + 50 for i in range(46)]
-counts = {'000':0,'001':0,'010':0,'011':0,'100':0,'101':0,'110':0,'111':0}
+# all the probabilities are added up here to yield the quantum rank
+rank = {'000':0,'001':0,'010':0,'011':0,'100':0,'101':0,'110':0}
 for run in runs:
 
-    q = QuantumRegister(4)
-    c = ClassicalRegister(3)
-    qc = QuantumCircuit(q,c)
+    # initialize qc
+    qc = QuantumCircuit(n_qubits+1,n_qubits)
 
-    qc.initialize(init_vector, [0,1,2,3])
+    qc.initialize(init_vector, [i for i in range(n_qubits+1)])
 
+    # add on appropriate #'s of C and S gates
     for i in range(run):
-        qc.append(c_gate, [0,1,2,3])
-        qc.append(s_gate, [0,1,2,3])
-    # add on subcircuit from unitary
-    qc.measure([0,1,2], [0,1,2])
+        qc.append(c_gate, [i for i in range(n_qubits+1)])
+        qc.append(s_gate, [i for i in range(n_qubits+1)])
 
+    # measure output probabilities
+    # we're 90% sure that the last qubit is the coin
+    qc.measure([i for i in range(n_qubits)], [i for i in range(n_qubits)])
+
+    # run circuit and store output probabilities
     simulator = Aer.get_backend("qasm_simulator")
     job = execute(qc, simulator, shots=5000)
     result = job.result()
     counts_run = result.get_counts(qc)
+    # add up probabilities for all runs
     for key in counts_run.keys():
-        counts[key] += counts_run[key]
-plot_histogram(counts)
+        rank[key] += counts_run[key]
 
-# draw circuit
-# qc.draw(output="mpl")
+# normalize & plot quantum rank
+rank = rank/sum(rank.values())
+plot_histogram(rank)
 
+# %% codecell
 ############################
 # DECOMPOSING UNITARY NOT WORKING
 ############################
@@ -89,5 +87,16 @@ plot_histogram(counts)
 # decomp.append(c_gate.definition, [0,1,2,3])
 # decomp.append(s_gate.definition, [0,1,2,3])
 # decomp.decompose().draw(output='mpl')
-# %% codecell
-# decompose ckt and draw
+
+# # %% codecell
+########################
+# I don't think this block works, but it may be useful later
+# Makes a subcircuit out of a UnitaryGate
+########################
+
+# # make a circuit with this gate
+#
+# testckt=unit_gate.definition
+# testckt.name = 'test'
+# testckt.draw(output='text')
+########################
