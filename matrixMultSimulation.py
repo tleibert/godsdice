0bdf2022da44eda5ecc0bf0ef3daabbe35b6ad0b
Kyle Connelly <kmconnel@ncsu.edu>
2020-06-30 18:32:18 -0400

    standard quantum walk

--- matrixMultSimulation.py
+++ matrixMultSimulation.py
@@ -123,30 +123,30 @@ def scaleFree(nodes):
     return adj_mat
 
 # %% codecell
-# # 7-node graph from paper
-# A = np.array([  [0,1,0,0,1,1,1,0],
-#                 [0,0,0,0,0,0,0,0],
-#                 [1,1,0,0,0,0,1,0],
-#                 [0,0,1,0,1,1,0,0],
-#                 [0,0,0,0,0,0,1,0],
-#                 [0,0,1,0,0,0,0,0],
-#                 [0,0,0,0,1,0,0,0],
-#                 [0,0,0,0,0,0,0,0]])
+# 7-node graph from paper
+A = np.array([  [0,1,0,0,1,1,1,0],
+                [0,0,0,0,0,0,0,0],
+                [1,1,0,0,0,0,1,0],
+                [0,0,1,0,1,1,0,0],
+                [0,0,0,0,0,0,1,0],
+                [0,0,1,0,0,0,0,0],
+                [0,0,0,0,1,0,0,0],
+                [0,0,0,0,0,0,0,0]])
 
 # 5-level binary tree
 
 # A = binaryTree(5)
 
 # 6-qubit scale-free network
-A = scaleFree(32)
+# A = scaleFree(32)
 
 # initialize statevector, operators
 C = constructC(A)
 S = constructS(A)
 SC = S @ C
 
-initialState = np.zeros(32)
-active_nodes = 32
+initialState = np.zeros(8)
+active_nodes = 7
 
 for i in range(active_nodes):
     initialState[i] = 1/np.sqrt(active_nodes)
@@ -168,146 +168,13 @@ for item in range(len(finalStates)):
 # combine instantaneous ranks into final average rank
 # creates as many dictionary slots as we need
 counts = {}
-for i in range(2**5):
-    counts[bin(i)[2:].zfill(5)] = 0
+for i in range(2**3):
+    counts[bin(i)[2:].zfill(3)] = 0
 
 for item in probabilityList[50:]:
     for i in range(len(item)):
         # figures out the binary label that qiskit would return
-        binI = bin(i)[2:].zfill(6)[1:]
+        binI = bin(i)[2:].zfill(4)[1:]
         counts[binI] += item[i]
 
 plot_histogram(counts)
-
-# %% codecell
-
-# #Finding C
-# C = constructC(A)
-# C = np.matrix(C)
-#
-# S = constructS(A)
-# S = np.matrix(S)
-#
-"""
-Created on Sat Jun 27 17:15:34 2020
-
-@author: Thoma
-
-
-import numpy as np
-import math
-import numpy.linalg as la
-import scipy.linalg as scila
-from numpy import kron
-
-#Adjaceny standard of Aij is a line from i to j
-#kronBuilder can be fixed
-
-def indegrees(M,node):
-    #This is the sum of the colume at node X, counting from 0
-    sumCurrent = 0
-    for index in range(0,M.shape[0]):
-        sumCurrent += M[index][node]
-    return sumCurrent
-
-def outdegrees(M,node):
-    #Counts the entries in the node row
-    return sum(M[node])
-
-def aVal(M,x):
-    indegree = indegrees(M,x)
-    outdegree = outdegrees(M,x)
-    if indegree == 0 and outdegree == 0:
-        return 1
-    else:
-        return indegree / (indegree + outdegree)
-
-def kronBuilder(stringElement,length):
-    stringElement = stringElement.zfill(length)
-    kronReturn = 1
-    kronIndex = [[1,0],[0,1]]
-    for item in range(length):
-        kronReturn = kron(kronIndex[int(stringElement[item])],kronReturn)
-    return kronReturn
-    #print(kronReturn)
-
-def constructC(M):
-    dimensions = math.ceil(np.log2(M.shape[0]))
-    output = np.zeros([2 * x for x in M.shape])
-
-    for x in range(M.shape[0]):
-        xVec = np.zeros(M.shape[0])
-        xVec[x] = 1
-        outerX = np.outer(xVec,xVec)
-        a = aVal(M,x)
-        cellElement = np.array([[np.sqrt(1/(a + 1)),np.sqrt(a/(1 + a))],[np.sqrt(a/(1 + a)),-1 * np.sqrt(1/(a + 1))]])
-        output += kron(cellElement,outerX)
-    return output
-
-
-#WIP
-def constructS(M,U):
-    dimensions = math.ceil(np.log2(M.shape[0]))
-    output = np.zeros([2 * x for x in M.shape],dtype=np.complex128)
-    up = np.array([1,0])
-    down = np.array([0,1])
-
-    for x in range(M.shape[0]):
-        xVec = np.zeros(M.shape[0])
-        xVec[x] = 1
-        outerX = np.outer(xVec, xVec)
-        firstTerm = kron(np.outer(up,up),outerX)
-        secondTermOutput = np.zeros([2 * x for x in M.shape],dtype=np.complex128)
-        for k in range(M.shape[0]):
-            kVec = np.zeros(M.shape[0])
-            kVec[k] = 1
-            kOuterX = np.outer(kVec,xVec)
-            outerDown = np.outer(down,down)
-            #CHECK THESE COORDS
-            secondTermOutput += kron(outerDown,U[k][x] * kOuterX)
-        output += firstTerm
-        output += secondTermOutput
-    return output
-
-
-#For the test case:
-#A = np.array([[0,1,0,0,1,1,1,0],[0,0,0,0,0,0,0,0],[1,1,0,0,0,0,1,0],[0,0,1,0,1,1,0,0],[0,0,0,0,0,0,1,0],[0,0,1,0,0,0,0,0],[0,0,0,0,1,0,0,0],[0,0,0,0,0,0,0,0]])
-#PDQ = la.svd(A)
-#P = PDQ[0]
-#D = np.diag(PDQ[1])
-#Q = PDQ[2]
-#U = P @ scila.expm(1j*D) @ Q
-
-
-#print(indegrees(A,1))
-#print(outdegrees(A,1))
-#print(indegrees(A,0))
-#print(outdegrees(A,0))
-
-#Finding C
-#C = constructC(A)
-#C = np.matrix(C)
-#print(np.round(C.H @ C,3))
-#print(C)
-#S = constructS(A,U)
-#S = np.matrix(S)
-#print(np.round(S.H @ S,3).real)
-'''
-print(la.det(constructC(A)))
-print(la.det(constructS(A,U)))
-U = np.matrix(U)
-print(np.round(U.H @ U,3))
-print(la.det(U))
-
-'''
-
-
-
-#xTest = kronBuilder('111',3)
-#print(np.zeros([2 * x for x in A.shape]))
-#print(np.outer(xTest))
-
-#print(kronBuilder('111',3))
-#print(math.ceil(np.log2(A.shape[0])))
-
-"""
---
+++ quantumWalk.py
@@ -0,0 +1,117 @@
+# %% codecell
+# initialize
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit import Aer, execute
+from qiskit.extensions import UnitaryGate
+from qiskit.visualization import plot_histogram
+import numpy as np
+from numpy import pi
+import matplotlib.pyplot as plt
+import scipy.linalg as la
+
+# %% codecell
+# define gates
+def qft(n_qubits):
+
+    qc = QuantumCircuit(n_qubits, name='QFT')
+
+    for i in range(n_qubits):
+        qc.h(-1-i)
+        for j in range(1,n_qubits-i):
+            qc.cu1(pi/(2**j), -1-(i+j), -1-i)
+        qc.barrier([i for i in range(n_qubits)])
+
+    # usually you would swap the order of the qubits here - since we'll be
+    # inverting the QFT at the end, it's cheaper to leave that out and just swap
+    # the manipulations in between QFT and QFT_dag
+
+    return qc
+
+def step(n_qubits):
+    # if heads (|0>) move +1, if tails (|1>), -1
+    # n_qubits INCLUDES the coin qubit - coin is the last one
+
+    qc = QuantumCircuit(n_qubits, name='Step')
+    theta = 2*pi/(2**(n_qubits-1))
+
+    for i in range(n_qubits-1):
+        qc.rz((2**i)*theta, n_qubits-i-2)
+        qc.crz(-2*(2**i)*theta, n_qubits-1, n_qubits-i-2)
+    # fudge phase factor
+    qc.rz(theta, -1)
+
+    return qc
+
+# step written out with matrix multiplication instead of QFT
+def adder_step(n_qubits):
+
+    add_mat = np.zeros([2**(n_qubits-1),2**(n_qubits-1)])
+    add_mat[0,-1] = 1
+    for i in range(2**(n_qubits-1)-1):
+        add_mat[i+1,i]=1
+
+    sub_mat = np.zeros([2**(n_qubits-1),2**(n_qubits-1)])
+    sub_mat[-1,0] = 1
+    for i in range(2**(n_qubits-1)-1):
+        sub_mat[i,i+1]=1
+
+    up_proj = np.diag([1,0])
+    down_proj = np.diag([0,1])
+
+    step = np.kron(up_proj,add_mat) + np.kron(down_proj,sub_mat)
+
+    return UnitaryGate(step)
+
+# %% codecell
+# Quantum Walk circuit
+
+n_qubits = 7 # THIS INCLUDES THE COIN - coin is the last qubit
+n_steps = 25 # number of timesteps
+
+# pick either 'statevector_simulator' or 'qasm_simulator'
+backend = 'statevector_simulator'
+
+qc = QuantumCircuit(n_qubits, n_qubits-1)
+# start in the middle of the chain, coin heads up
+qc.x(-2)
+
+qc.append(qft(n_qubits-1), [i for i in range(n_qubits-1)])
+for i in range(n_steps):
+    # flip the coin
+    qc.h(-1)
+    # take a step
+    qc.append(step(n_qubits), [i for i in range(n_qubits)])
+qc.append(qft(n_qubits-1).inverse(), [i for i in range(n_qubits-1)])
+if backend is 'qasm_simulator':
+    qc.measure([i for i in range(n_qubits-1)], [i for i in range(n_qubits-1)])
+
+# for i in range(n_steps):
+#     qc.h(-1)
+#     qc.append(adder_step(n_qubits), [i for i in range(n_qubits)])
+
+# %% codecell
+# execute the circuit & plot output
+
+simulator = Aer.get_backend(backend)
+job = execute(qc, simulator)
+
+if backend is 'qasm_simulator':
+    counts_ex = job.result().get_counts()
+    # make the histogram include zeros
+    counts = {}
+    for i in range(2**(n_qubits-1)):
+        key = bin(i)[2:].zfill(n_qubits-1)
+        counts[key] = 0
+    for key in counts_ex.keys():
+        counts[key] = counts_ex[key]
+        plot_histogram(counts, bar_labels=False)
+
+if backend is 'statevector_simulator':
+    psi_final = job.result().get_statevector()
+    # combine probability of x & heads with x & tails
+    prob_final = [np.abs(psi_final[i])**2 + np.abs(psi_final[i + 2**(n_qubits-1)])**2
+     for i in range(2**(n_qubits-1))]
+    plt.plot(np.arange(2**(n_qubits-1)) - 2**(n_qubits-2), prob_final)
+
+    # plt.matshow(np.real(np.diag(psi_final)))
+    # plt.matshow(np.imag(np.diag(psi_final)))
