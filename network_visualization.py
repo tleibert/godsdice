c01e47379f831feaf5843afd256244fe32f72bef
Drew Connelly <67079673+ArthurDentt@users.noreply.github.com>
2020-07-01 10:54:39 -0400

    Update network_visualization.py

--- network_visualization.py
+++ network_visualization.py
@@ -5,51 +5,6 @@ import networkx as nx
 
 import pageRankSim
 
-# Some example run code for pageRankSim below
-"""
-#initialize run conditions
-n_runs = 10 # number of runs in final iteration
-shots = 50 # number of shots on IBM qasm simulator
-n_frames = 10 # number of frames in gif n_runs >= n_frames or else it wont work
-
-#Adjacency Matrix
-
-A = np.array([  [0,1,0,0,1,1,0,0],
-                [0,0,0,0,0,0,0,0],
-                [1,1,0,0,0,0,1,0],
-                [0,0,1,0,1,1,0,0],
-                [0,0,0,0,0,0,1,0],
-                [0,0,0,1,0,0,0,0],
-                [0,0,0,0,0,1,0,0],
-                [0,0,0,0,0,0,0,0]])
-
-# for graph node 8 disconnected : index = 7
-disc_indices = [7]
-
-nqubits=int(np.log2(A.shape[0]))
-disc_indices2 = np.zeros(len(disc_indices))
-
-# Using indices from disconnected vertices to decide complementary indices from
-# left-concatenating 1 to the binary number
-for i in range(len(disc_indices)):
-    disc_indices2[i]=disc_indices[i] + 2**nqubits
-
-n_states = 2**nqubits - len(disc_indices)
-initsize = A.shape[0]*2
-init_vector = np.ones(initsize)/np.sqrt(initsize-2*len(disc_indices))
-
-    # making parts of init vector =0
-for i in range(len(disc_indices)):
-    init_vector[int(disc_indices[i])] = 0
-    init_vector[int(disc_indices2[i])] = 0
-
-rankmatrix=np.zeros((n_frames,n_states))
-
-for i in range(1,n_frames+1,1):
-    rankrow = pageRankSim.simulate(A,init_vector,int((n_runs/n_frames)*i),shots)
-    rankmatrix[i-1,:]=rankrow
-"""
-
 def connected_node_mask(A):
     node_mask = [] # true for connected nodes, false for disconnected nodes
     for i in range(A.shape[0]):
