2d68d782e1c674f28e24110b6bccdc67b51e55cc
noahewolfe <noahwolfe1999@gmail.com>
2020-06-30 22:05:41 -0400

    Changed boolean comparison to assignment

--- classical_walk.py
+++ classical_walk.py
@@ -15,7 +15,7 @@ def connected_node_mask(A):
 def cwalk(N, R):
     # each row corresponds to a trial R
     coin_flips = np.random.randint(0,2,(R,N))
-    coin_flips[ coin_flips == 0 ] == -1 # set all 0's to -1's
+    coin_flips[ coin_flips == 0 ] = -1 # set all 0's to -1's
     return np.sum(coin_flips,axis=1)
 
     """
