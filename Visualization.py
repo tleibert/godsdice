fd820ff1ebcd8a7a66fa816c7566a7cdd2ff5fc2
Noah Wolfe <noahwolfe1999@gmail.com>
2020-06-30 20:22:55 -0400

    added colored network graph

--- Visualization.py
+++ Visualization.py
@@ -9,8 +9,7 @@ from qiskit.visualization import plot_histogram
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg as la
-import Testing_Unitaries
-
+import pageRankSim
 import matplotlib.animation as animation
 ######################
 # MAKE SURE Testing_Unitaries IS UP TO DATE!
@@ -20,12 +19,12 @@ import matplotlib.animation as animation
 # create the operators
 
 #initialize run conditions
-n_runs = 50
-shots = 500
-# for graph node 8 disconnected : index = 7
-disc_indices = [7]
+n_runs = 10 # number of runs in final iteration
+shots = 50 # number of shots on IBM qasm simulator
+n_frames = 10 # number of frames in gif n_runs >= n_frames or else it wont work
 
 #Adjacency Matrix
+
 A = np.array([  [0,1,0,0,1,1,0,0],
                 [0,0,0,0,0,0,0,0],
                 [1,1,0,0,0,0,1,0],
@@ -34,28 +33,52 @@ A = np.array([  [0,1,0,0,1,1,0,0],
                 [0,0,0,1,0,0,0,0],
                 [0,0,0,0,0,1,0,0],
                 [0,0,0,0,0,0,0,0]])
-initsize = A.shape[1]*2
+
+# for graph node 8 disconnected : index = 7
+disc_indices = [7]
+
+nqubits=int(np.log2(A.shape[0]))
+disc_indices2 = np.zeros(len(disc_indices))
+
+# Using indices from disconnected vertices to decide complementary indices from
+# left-concatenating 1 to the binary number
+for i in range(len(disc_indices)):
+    disc_indices2[i]=disc_indices[i] + 2**nqubits
+
+n_states = 2**nqubits - len(disc_indices)
+initsize = A.shape[0]*2
 init_vector = np.ones(initsize)/np.sqrt(initsize-2*len(disc_indices))
+
+    # making parts of init vector =0
+for i in range(len(disc_indices)):
+    init_vector[int(disc_indices[i])] = 0
+    init_vector[int(disc_indices2[i])] = 0
 
 # initialize to superposition without |up>|111> or |down>|111>
 
-init_vector[7] = init_vector[15] = 0
+rankmatrix=np.zeros((n_frames,n_states))
 
-
-# %% codecell
-
-rankmatrix = A
+for i in range(1,n_frames+1,1):
+    rankrow = pageRankSim.simulate(A,init_vector,int((n_runs/n_frames)*i),shots)
+    rankmatrix[i-1,:]=rankrow
 
 fig, ax = plt.subplots()
+
+
+xpos = np.arange(1,rankmatrix[0].shape[0]+1)
+ax.set_xticks( xpos, [ str(x) for x in xpos ]) # name them 1 - 7
+
+hist_ylim = np.min([ np.around(np.max(rankmatrix), decimals=1) + 0.05, 1.0 ])
 
 # animation
 def update_hist(row, data):
     ax.clear()
-    ### This will fix the y-axis; can adjust the upperbound 0.5 as nessecary
-    ax.set_ylim(0,0.5) ### Uncomment when we're using a real array, not A, for plotting
-    ax.hist(data[row,:])
+    ax.set_ylim(0, hist_ylim )
+    ax.bar(xpos, data[row,:])
+    for x in xpos:
+        ax.text( x - 0.325, data[row,x-1] + 0.005, "%.3f" % data[row,x-1] )
 
-hist = ax.hist(rankmatrix[0,:])
+ax.bar(xpos, rankmatrix[0,:])
 
 anim = animation.FuncAnimation(fig, update_hist, rankmatrix.shape[0], fargs=(rankmatrix, ) )
 
